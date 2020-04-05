ab766aa1730ac7536cd3d70ed81952f1b1271190
Josearratia <1630037@upv.edu.mx>
2020-04-05 03:01:31 -0500

    subida de archivos con jq

--- forms/upload.php
+++ forms/upload.php
@@ -1,18 +1,43 @@
 <?php
 
-/* include_once 'user.php'; */
+include_once 'user.php';  
 include_once 'Session.php';
 
 $userSession = new UserSession();
-/* $user = new login(); */
+$user = new login(); 
 
-/* if (isset($_SESSION['user'])) {
+ if (isset($_SESSION['user'])) {
     $user->setUserAndfk($userSession->getCurrentUser());
-    $directorio = "assets/upload/img/uploadimg/";
+    $directorio =  $_SERVER['DOCUMENT_ROOT'] ."/assets/upload/img/uploadimg/";
+    /* echo var_dump($_FILES); */
+    $tipoArchivo = strtolower(pathinfo($_FILES['mp']["name"], PATHINFO_EXTENSION));
+    $archivo = $directorio . basename($user->getusercode()) . '.' . $tipoArchivo;
+    
+    $isimg = getimagesize($_FILES["mp"]["tmp_name"]);
 
-    $archivo = $directorio . basename($user->getusercode());
-    echo $archivo;
+    if($isimg != false){
+        $size = $_FILES["mp"]["size"];
+
+        if($size > 10000000){
+            echo "Archivo mayor a 10mb";
+        }else {
+            if($tipoArchivo == "jpg"){
+
+                if(move_uploaded_file($_FILES["mp"]["tmp_name"],$archivo)){
+                    echo "Archivo guardado";
+                }else{
+                    echo "error al guardar el archivo";
+                }
+
+            }else{
+                echo "solo se permitente archivos jpg";
+            }
+        }
+
+    }else {
+        echo "Archivo no valido";
+    }
 } else {
     include_once 'index.php';
-} */
+} 
     
