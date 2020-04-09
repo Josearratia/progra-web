bdf2f83af673e58c86e011612419f6bc5b04757b
Josearratia <1630037@upv.edu.mx>
2020-04-09 11:27:55 -0500

    ordenado de categorias roles

--- roles/view_roles.php
+++ roles/view_roles.php
@@ -5,9 +5,9 @@
                     <div class="card-body">
                         <h1>Categoria Roles</h1>
                         <?php  if($rolesaccess->getrolaccess_addroles() == 1){include_once 'view_agregarroles.php'; } ?>
+                        <?php  if($rolesaccess->getrolaccess_modificarroles() == 1){include_once 'view_modificarroles.php'; } ?>
                         <?php  if($rolesaccess->getrolaccess_asignarroles() == 1){include_once 'view_asignarroles.php'; } ?>
                         <?php  if($rolesaccess->getrolaccess_deletroles() == 1){include_once 'view_eliminarroles.php'; } ?>
-                        <?php  if($rolesaccess->getrolaccess_modificarroles() == 1){include_once 'view_modificarroles.php'; } ?>
                     </div>
                 </div>
             </div>
